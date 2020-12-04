fa541da57609e4ad137816cfb7a62bbed8c36137
Casper Rysgaard <casperrysgaard@gmail.com>
2020-12-04 13:13:41 +0100

    fixup on 2020 day 4

--- y2020/Day4.py
+++ y2020/Day4.py
@@ -27,6 +27,52 @@
 # print(valid)
 
 
+# Part 2
+# valid = 0
+# currHave = 0
+# while True:
+#     line = input()
+#     if line == "-1":
+#         break
+#     if line == "":
+#         valid += currHave == 7
+#         currHave = 0
+#     else:
+#         for dat in line.split():
+#             key, value = dat.split(":")
+#             if key == "byr":
+#                 currHave += (1920 <= int(value) <= 2002)
+#             elif key == "iyr":
+#                 currHave += (2010 <= int(value) <= 2020)
+#             elif key == "eyr":
+#                 currHave += (2020 <= int(value) <= 2030)
+#             elif key == "hgt":
+#                 h = int("0" + value[:-2])
+#                 if value[-2:] == "cm":
+#                     currHave += (150 <= h <= 193)
+#                 elif value[-2:] == "in":
+#                     currHave += (59 <= h <= 76)
+#             elif key == "hcl":
+#                 currHave += (value[0] == "#" and len(value) == 7 and all(v in "0123456789abcdef" for v in value[1:]))
+#             elif key == "ecl":
+#                 currHave += (value in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"})
+#             elif key == "pid":
+#                 currHave += (len(value) == 9 and all(v in "0123456789" for v in value))
+#
+# print(valid)
+
+conditions = {
+    "byr": lambda value: 1920 <= int(value) <= 2002,
+    "iyr": lambda value: 2010 <= int(value) <= 2020,
+    "eyr": lambda value: 2020 <= int(value) <= 2030,
+    "hgt": lambda value: value[-2:] == "cm" and (150 <= int("0" + value[:-2]) <= 193) or \
+                         value[-2:] == "in" and (59 <= int("0" + value[:-2]) <= 76),
+    "hcl": lambda value: value[0] == "#" and len(value) == 7 and all(v in "0123456789abcdef" for v in value[1:]),
+    "ecl": lambda value: value in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"},
+    "pid": lambda value: len(value) == 9 and all(v in "0123456789" for v in value),
+    "cid": lambda value: False
+}
+
 valid = 0
 currHave = 0
 while True:
@@ -39,24 +85,6 @@ while True:
     else:
         for dat in line.split():
             key, value = dat.split(":")
-            if key == "byr":
-                currHave += (1920 <= int(value) <= 2002)
-            elif key == "iyr":
-                currHave += (2010 <= int(value) <= 2020)
-            elif key == "eyr":
-                currHave += (2020 <= int(value) <= 2030)
-            elif key == "hgt":
-                h = int("0" + value[:-2])
-                if value[-2:] == "cm":
-                    currHave += (150 <= h <= 193)
-                elif value[-2:] == "in":
-                    currHave += (59 <= h <= 76)
-            elif key == "hcl":
-                currHave += (value[0] == "#" and len(value) == 7 and all(v in "0123456789abcdef" for v in value[1:]))
-            elif key == "ecl":
-                currHave += (value in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"})
-            elif key == "pid":
-                currHave += (len(value) == 9 and all(v in "0123456789" for v in value))
-            # print(key, value, currHave)
+            currHave += conditions[key](value)
 
 print(valid)
